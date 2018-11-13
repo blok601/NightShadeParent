c91681be130e797a89dad59bd14f253baacbc62d
blok601 <dhillonchoudhri@gmail.com>
2018-11-13 18:37:14 -0500

    Scoreboard tweaks

--- NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/scoreboard/provider/type/UHCProvider.java
+++ NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/scoreboard/provider/type/UHCProvider.java
@@ -34,16 +34,16 @@ public class UHCProvider extends ScoreboardProvider {
         UHCPlayer uhcPlayer = UHCPlayer.get(p);
         List<ScoreboardText> lines = new ArrayList<>();
 
-        lines.add(new ScoreboardText(ChatUtils.format("&f&m--------------------")));
-        lines.add(new ScoreboardText(ChatUtils.format("&3Game Clock: &e" + (GameManager.getTimer().isRunning() ? GameManager.getTimer().getTime() : "Waiting..."))));
-        lines.add(new ScoreboardText(ChatUtils.format("&3Kills: &e" + GameManager.getKills().getOrDefault(p.getUniqueId(), 0))));
+        lines.add(new ScoreboardText(ChatUtils.format("&3&m--------------------")));
+        lines.add(new ScoreboardText(ChatUtils.format("&fGame Clock: &e" + (GameManager.getTimer().isRunning() ? GameManager.getTimer().getTime() : "Waiting..."))));
+        lines.add(new ScoreboardText(ChatUtils.format("&fKills: &e" + GameManager.getKills().getOrDefault(p.getUniqueId(), 0))));
         if (GameManager.isIsTeam()) {
             //Team game
             Team team = TeamManager.getInstance().getTeam(p);
 
 
             if (team == null) {
-                lines.add(new ScoreboardText(ChatUtils.format("&3Team Kills: &e0")));
+                lines.add(new ScoreboardText(ChatUtils.format("&fTeam Kills: &e0")));
             } else {
                 //They have a team now
                 int teamKills = 0;
@@ -56,26 +56,26 @@ public class UHCProvider extends ScoreboardProvider {
                         teamKills += GameManager.getKills().getOrDefault(Bukkit.getPlayer(member).getUniqueId(), 0);
                     }
                 }
-                lines.add(new ScoreboardText(ChatUtils.format("&3Team Kills: &e" + teamKills)));
+                lines.add(new ScoreboardText(ChatUtils.format("&fTeam Kills: &e" + teamKills)));
             }
         }
         if(GameState.gameHasStarted()){
-            lines.add(new ScoreboardText(ChatUtils.format("&3Players: &e") + UHC.players.size() + "/" + GameManager.getMaxPlayers()));
+            lines.add(new ScoreboardText(ChatUtils.format("&fPlayers: &e") + UHC.players.size() + "/" + GameManager.getMaxPlayers()));
         }else{
-            lines.add(new ScoreboardText(ChatUtils.format("&3Players: &e" + (Bukkit.getOnlinePlayers().size() - UHCPlayerColl.get().getAllOnline().stream().filter(UHCPlayer::isSpectator).count()))));
+            lines.add(new ScoreboardText(ChatUtils.format("&fPlayers: &e" + (Bukkit.getOnlinePlayers().size() - UHCPlayerColl.get().getAllOnline().stream().filter(UHCPlayer::isSpectator).count()))));
         }
 
-        lines.add(new ScoreboardText(ChatUtils.format("&3Spectators: &e" + UHCPlayerColl.get().getAllOnline().stream().filter(UHCPlayer::isSpectator).count())));
+        lines.add(new ScoreboardText(ChatUtils.format("&fSpectators: &e" + UHCPlayerColl.get().getAllOnline().stream().filter(UHCPlayer::isSpectator).count())));
         if (GameManager.getWorld() == null) {
-            lines.add(new ScoreboardText(ChatUtils.format("&3Border: &eWorld Not Set")));
+            lines.add(new ScoreboardText(ChatUtils.format("&fBorder: &eWorld Not Set")));
         } else {
             //Have a world
-            lines.add(new ScoreboardText(ChatUtils.format("&3Border: &e" + ((int) GameManager.getBorderSize()))));
+            lines.add(new ScoreboardText(ChatUtils.format("&fBorder: &e" + ((int) GameManager.getBorderSize()))));
         }
-        lines.add(new ScoreboardText(ChatUtils.format("&f&m--------------------&r")));
+        lines.add(new ScoreboardText(ChatUtils.format("&3&m--------------------&r")));
         if(uhcPlayer.isNoClean()){
-            lines.add(new ScoreboardText(ChatUtils.format("&3NoClean: &e" + uhcPlayer.getNoCleanTimer() + "s")));
-            lines.add(new ScoreboardText(ChatUtils.format("&f&m--------------------&r")));
+            lines.add(new ScoreboardText(ChatUtils.format("&fNoClean: &e" + uhcPlayer.getNoCleanTimer() + "s")));
+            lines.add(new ScoreboardText(ChatUtils.format("&3&m--------------------&r")));
         }
         lines.add(new ScoreboardText(ChatUtils.format("&ediscord.me/NightShadeMC")));
         return lines;
