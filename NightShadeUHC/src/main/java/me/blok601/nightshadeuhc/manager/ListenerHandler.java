00f51b6baae58b1e78f1ad2cb5fdf7919b45ba21
blok601 <dhillonchoudhri@gmail.com>
2018-12-17 17:37:38 -0500

    now i made a oopsie

--- NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/manager/ListenerHandler.java
+++ NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/manager/ListenerHandler.java
@@ -1,20 +1,17 @@
 package me.blok601.nightshadeuhc.manager;
 
-import me.blok601.nightshadeuhc.command.staff.RatesCommand;
 import me.blok601.nightshadeuhc.command.player.disguise.Undisguise;
+import me.blok601.nightshadeuhc.command.staff.RatesCommand;
 import me.blok601.nightshadeuhc.component.GoldenHeadConsume;
 import me.blok601.nightshadeuhc.listener.game.*;
 import me.blok601.nightshadeuhc.listener.gui.GameSetupInventoryClick;
 import me.blok601.nightshadeuhc.listener.gui.InvClick;
 import me.blok601.nightshadeuhc.listener.gui.ScenarioClick;
-import me.blok601.nightshadeuhc.listener.game.LoggerListener;
-import me.blok601.nightshadeuhc.scenario.SkriptScenarios;
-import me.blok601.nightshadeuhc.staff.listener.CommandSpyListener;
-import me.blok601.nightshadeuhc.listener.misc.StaffListener;
-import me.blok601.nightshadeuhc.listener.misc.SpectatorListener;
 import me.blok601.nightshadeuhc.listener.misc.SpectatorInfoListener;
+import me.blok601.nightshadeuhc.listener.misc.SpectatorListener;
+import me.blok601.nightshadeuhc.listener.misc.StaffListener;
+import me.blok601.nightshadeuhc.scenario.SkriptScenarios;
 import me.blok601.nightshadeuhc.stat.listener.JoinListener;
-import me.blok601.nightshadeuhc.teams.FriendlyFire;
 import org.bukkit.event.Listener;
 
 /**
@@ -26,17 +23,14 @@ public class ListenerHandler {
             new LoggerListener(),
             new InvClick(),
             new SpectatorListener(),
-            new FriendlyFire(),
             new GoldenHeadConsume(),
             new RatesCommand(),
             ScenarioClick.get(),
             new StaffListener(),
             new Undisguise(),
-            new CommandSpyListener(),
             new JoinListener(),
             new GameListener(),
             new PlayerListener(),
-            new BorderActionBar(),
             new SkriptScenarios(),
             new ArenaListener(),
             new SpectatorInfoListener(),
