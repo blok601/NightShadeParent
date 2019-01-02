f77bb5201f0e82cef47aebe8eab3d229a8447aad
blok601 <dhillonchoudhri@gmail.com>
2019-01-02 15:29:09 -0500

    Small changes

--- NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/listener/game/GameDeathListener.java
+++ NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/listener/game/GameDeathListener.java
@@ -158,14 +158,23 @@ public class GameDeathListener implements Listener {
             }
             //points += uhcPlayer.getChangedLevel();
 
+            DecimalFormat decimalFormat = new DecimalFormat("##.##");
             double curr = GameManager.get().getPointChanges().get(uhcPlayer.getUuid());
             GameManager.get().getPointChanges().put(uhcPlayer.getUuid(), curr + points);
+            String changed = decimalFormat.format(curr + points);
             uhcPlayer.addPoints(points);
             uhcPlayer.changed();
 
-
             p.sendMessage(ChatUtils.message("&eYou have died! Thank you for playing on NightShadePvP!"));
             p.sendMessage(ChatUtils.message("&eJoin the Discord at discord.me/NightShadePvP for updates and more!"));
+            p.sendMessage(ChatUtils.format("&f&m----------------------------"));
+            p.playSound(p.getLocation(), Sound.LEVEL_UP, 5F, 5F);
+            p.sendMessage(ChatUtils.message("&bYour Game Stats:"));
+            p.sendMessage(ChatUtils.format("      " + (curr + points >= 0 ? "&a&o+" + changed + " points" : "&c&o-" + changed + " points")));
+            p.sendMessage(ChatUtils.format("      &bCurrent Points: &f" + decimalFormat.format(uhcPlayer.getPoints())));
+            p.sendMessage(ChatUtils.format("      &bKills: " + GameManager.get().getKills().get(uhcPlayer.getUuid())));
+            p.sendMessage(ChatUtils.format("&f&m----------------------------"));
+            GameManager.get().getPointChanges().remove(p.getUniqueId());
 
             if (user.hasRank(Rank.DRAGON)) { //Dragon and above can spectate the games, otherwise kick
                 p.setAllowFlight(true);
@@ -307,6 +316,7 @@ public class GameDeathListener implements Listener {
         p.sendMessage(ChatUtils.format("      &bCurrent Points: &f" + decimalFormat.format(uhcPlayer.getPoints())));
         p.sendMessage(ChatUtils.format("      &bKills: " + GameManager.get().getKills().get(uhcPlayer.getUuid())));
         p.sendMessage(ChatUtils.format("&f&m----------------------------"));
+        GameManager.get().getPointChanges().remove(p.getUniqueId());
 
         if (user.hasRank(Rank.YOUTUBE)) { //YouTube and above can spectate the games, otherwise kick
             p.setAllowFlight(true);
