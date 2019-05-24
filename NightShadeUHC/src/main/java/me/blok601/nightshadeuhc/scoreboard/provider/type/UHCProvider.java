3b0fa124119f8d0ccf34f421c56a5bd7bfc1d484
OzzyPG <39841508+OzzyPG@users.noreply.github.com>
2019-05-24 12:15:43 -0400

    revert

--- NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/scoreboard/provider/type/UHCProvider.java
+++ NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/scoreboard/provider/type/UHCProvider.java
@@ -22,7 +22,7 @@ public class UHCProvider extends ScoreboardProvider {
 
     @Override
     public String getTitle(Player p) {
-        return ChatUtils.format("&cShatterheart S1");
+        return ChatUtils.format("&5NightShade PvP");
     }
 
     /**
