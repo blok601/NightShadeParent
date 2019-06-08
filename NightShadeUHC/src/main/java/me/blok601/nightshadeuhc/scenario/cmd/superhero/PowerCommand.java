d0d23e41e762455758f98c76c73aadcc2ce12404
OzzyPG <39841508+OzzyPG@users.noreply.github.com>
2019-06-08 15:20:22 -0400

    C:/Program Files/Git/power now works for spectators

--- NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/scenario/cmd/superhero/PowerCommand.java
+++ NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/scenario/cmd/superhero/PowerCommand.java
@@ -1,11 +1,13 @@
 package me.blok601.nightshadeuhc.scenario.cmd.superhero;
 
 import com.nightshadepvp.core.Rank;
-import me.blok601.nightshadeuhc.entity.object.GameState;
 import me.blok601.nightshadeuhc.command.UHCCommand;
+import me.blok601.nightshadeuhc.entity.UHCPlayer;
+import me.blok601.nightshadeuhc.entity.object.GameState;
 import me.blok601.nightshadeuhc.scenario.ScenarioManager;
 import me.blok601.nightshadeuhc.scenario.SuperheroesScenario;
 import me.blok601.nightshadeuhc.util.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -31,23 +33,50 @@ public class PowerCommand implements UHCCommand{
     @Override
     public void onCommand(CommandSender s, Command cmd, String l, String[] args) {
         Player p = (Player) s;
-        if(!scenarioManager.getScen("Superheroes").isEnabled()){
+        if (!scenarioManager.getScen("Superheroes").isEnabled()) {
             p.sendMessage(ChatUtils.message("&cSuperheroes isn't enabled!"));
             return;
         }
 
-        if(!GameState.gameHasStarted()){
+        if (!GameState.gameHasStarted()) {
             p.sendMessage(ChatUtils.message("&cThe game hasn't started yet!"));
             return;
         }
 
-        if(!SuperheroesScenario.powers.containsKey(p.getUniqueId())) {
-            p.sendMessage(ChatUtils.message("&cYou haven't been assigned a power! Ask the host to assign you one!"));
-            return;
-        }
+        if (args.length == 0) {
 
-        SuperheroesScenario.SuperHeroType type = SuperheroesScenario.powers.get(p.getUniqueId());
-        p.sendMessage(ChatUtils.message("&eYour super power is: &3" + type.getName()));
+            if (!SuperheroesScenario.powers.containsKey(p.getUniqueId())) {
+                p.sendMessage(ChatUtils.message("&cYou haven't been assigned a power! Ask the host to assign you one!"));
+                return;
+            }
+
+            SuperheroesScenario.SuperHeroType type = SuperheroesScenario.powers.get(p.getUniqueId());
+            p.sendMessage(ChatUtils.message("&eYour super power is: &3" + type.getName()));
+        }
+        if (args.length == 1) {
+            UHCPlayer se = (UHCPlayer) p;
+            if (se.isSpectator()) {
+                Player target = Bukkit.getPlayer(args[1]);
+                if (target == null) {
+                    p.sendMessage(ChatUtils.message("&cThat player is not online!"));
+                    return;
+                }
+                if (!SuperheroesScenario.powers.containsKey(target.getUniqueId())) {
+                    p.sendMessage(ChatUtils.message("&cThat player has not be assigned a power!"));
+                    return;
+                }
+                SuperheroesScenario.SuperHeroType type = SuperheroesScenario.powers.get(target.getUniqueId());
+                p.sendMessage(ChatUtils.message("&e" + target.getName() + "'s Super power is: &3" + type.getName()));
+                return;
+
+            }
+            else {
+                p.sendMessage(ChatUtils.message("&cOnly Spectators can do this command!"));
+                return;
+            }
+
+
+        }
     }
 
     @Override
