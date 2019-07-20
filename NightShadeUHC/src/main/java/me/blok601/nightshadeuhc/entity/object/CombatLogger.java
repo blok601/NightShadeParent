76d78964289f0ce039a67549a99527a0cfd7282f
blok601 <dhillonchoudhri@gmail.com>
2019-07-20 18:30:34 -0400

    Update CombatLogger.java

--- NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/entity/object/CombatLogger.java
+++ NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/entity/object/CombatLogger.java
@@ -72,7 +72,7 @@ public class CombatLogger {
         //this.freeze();
 
         nameHologram = HologramAPI.createHologram(this.location.add(0, 1, 0), ChatUtils.format("&b&l" + loggerName + "'s Logger"));
-        nameHologram.setTouchable(false);
+        nameHologram.setTouchable(true);
         nameHologram.addLineBelow(ChatUtils.format("&a&l5:00"));
         nameHologram.spawn();
 
