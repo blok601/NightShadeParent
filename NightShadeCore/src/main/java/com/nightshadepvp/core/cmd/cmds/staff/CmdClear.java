ea073fe16acf319cfbb08754571a8d36958bc41d
dhillon <dhillonchoudhri@gmail.com>
2020-02-23 15:17:18 -0500

    Added new /clear command

---
+++ NightShadeCore/src/main/java/com/nightshadepvp/core/cmd/cmds/staff/CmdClear.java
@@ -0,0 +1,35 @@
+package com.nightshadepvp.core.cmd.cmds.staff;
+
+import com.massivecraft.massivecore.MassiveException;
+import com.massivecraft.massivecore.command.type.sender.TypePlayer;
+import com.nightshadepvp.core.Rank;
+import com.nightshadepvp.core.cmd.NightShadeCoreCommand;
+import com.nightshadepvp.core.cmd.req.ReqRankHasAtLeast;
+import com.nightshadepvp.core.entity.NSPlayer;
+import com.nightshadepvp.core.utils.ChatUtils;
+import org.bukkit.entity.Player;
+
+public class CmdClear extends NightShadeCoreCommand {
+
+    private static CmdClear i = new CmdClear();
+    public static CmdClear get() {return i;}
+
+    public CmdClear(){
+        this.addAliases("clear");
+        this.addParameter(TypePlayer.get(), "player", "you");
+        this.addRequirements(ReqRankHasAtLeast.get(Rank.TRIAL));
+    }
+
+    @Override
+    public void perform() throws MassiveException {
+        Player toClear = this.readArg(sender instanceof Player ? NSPlayer.get(sender).getPlayer() : null);
+        if(toClear == null || !toClear.isOnline()){
+            sender.sendMessage(ChatUtils.message("&cPlayer not found."));
+            return;
+        }
+
+        toClear.getInventory().setContents(null);
+        sender.sendMessage(ChatUtils.message("&bCleared the inventory of &f" + toClear.getName()));
+
+    }
+}
