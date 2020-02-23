31315114bf54c2ca88806a013c590c919efec7cb
dhillon <dhillonchoudhri@gmail.com>
2020-02-23 14:29:48 -0500

    Added tab complete to /punish

--- NightShadeCore/src/main/java/com/nightshadepvp/core/cmd/cmds/staff/CmdPunish.java
+++ NightShadeCore/src/main/java/com/nightshadepvp/core/cmd/cmds/staff/CmdPunish.java
@@ -6,7 +6,10 @@ import com.massivecraft.massivecore.command.type.primitive.TypeString;
 import com.nightshadepvp.core.Rank;
 import com.nightshadepvp.core.cmd.NightShadeCoreCommand;
 import com.nightshadepvp.core.cmd.req.ReqRankHasAtLeast;
+import com.nightshadepvp.core.cmd.type.TypeNSPlayer;
+import com.nightshadepvp.core.entity.NSPlayer;
 import com.nightshadepvp.core.punishment.PunishmentHandler;
+import com.nightshadepvp.core.utils.ChatUtils;
 import org.bukkit.entity.Player;
 
 /**
@@ -20,16 +23,38 @@ public class CmdPunish extends NightShadeCoreCommand{
 
     public CmdPunish() {
         this.addAliases("punish") ;
-        this.addParameter(TypeString.get(), "player");
+        this.addParameter(TypeNSPlayer.get(), "player");
         this.addRequirements(RequirementIsPlayer.get());
         this.addRequirements(ReqRankHasAtLeast.get(Rank.TRIAL));
+
     }
 
     @Override
     public void perform() throws MassiveException {
         Player p = (Player) sender;
-        String target = this.readArg();
-        PunishmentHandler.getInstance().getPunishing().put(p, target);
+        NSPlayer target = this.readArg();
+
+        if(target == null){
+            p.sendMessage(ChatUtils.message("&cThat player could not be found!"));
+            return;
+        }
+
+        if(!target.hasPlayedBefore()){
+            p.sendMessage(ChatUtils.message("&cThat player has never logged in before! Please contact an admin if this player needs to be punished!"));
+            return;
+        }
+
+        if(target.isConsole()){
+            p.sendMessage(ChatUtils.message("&cYou can't punish the console!"));
+            return;
+        }
+
+        if(target.hasRank(NSPlayer.get(p).getRank())){
+            p.sendMessage(ChatUtils.message("&cYou can't punish those with the same rank or higher as you."));
+            return;
+        }
+
+        PunishmentHandler.getInstance().getPunishing().put(p, target.getName());
         PunishmentHandler.getInstance().createGUI(p);
     }
 }
