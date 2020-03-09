c042adc319614b0d0c4405dabee6c39beea65b49
dhillon <dhillonchoudhri@gmail.com>
2020-03-08 20:11:43 -0400

    Started work on BattleArena

---
+++ NightShadeBattleArena/src/main/java/com/nightshadepvp/battlearena/BattleArena.java
@@ -0,0 +1,21 @@
+package com.nightshadepvp.battlearena;
+
+import com.massivecraft.massivecore.MassivePlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BattleArena extends MassivePlugin {
+
+    private static BattleArena i = new BattleArena();
+
+    public static BattleArena get(){return i;}
+
+    @Override
+    public void onEnableInner() {
+        this.activateAuto();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
