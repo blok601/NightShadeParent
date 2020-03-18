2f595d455fbf81c0e870447ad79c1de0b6267c46
OzzyPG <39841508+OzzyPG@users.noreply.github.com>
2020-03-18 11:20:00 -0400

    The final push

--- NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/command/player/teams/TeamBaseCommand.java
+++ NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/command/player/teams/TeamBaseCommand.java
@@ -229,10 +229,6 @@ public class TeamBaseCommand implements UHCCommand{
                     }
                     final Player target = Bukkit.getPlayerExact(args[1]);
                     Team t = TeamManager.getInstance().getTeam(target);
-                    for (String sl : t.getMembers()) {
-                        Player p1 = Bukkit.getPlayer(sl);
-                        t.removeColor(p1);
-                    }
                     TeamManager.getInstance().removeTeam(t);
                     p.sendMessage(ChatUtils.message("&cDeleted " + target.getName() + "&c's team!"));
 
