67ea20360c50fee848c27cc4ef6bb307a19e299e
dhillon <dhillonchoudhri@gmail.com>
2020-06-10 17:40:55 -0400

    Added champ detection back Added /bracket

---
+++ NightShadeTournament/src/main/java/com/nightshadepvp/tournament/cmd/cmds/player/CmdBracket.java
@@ -0,0 +1,28 @@
+package com.nightshadepvp.tournament.cmd.cmds.player;
+
+import com.massivecraft.massivecore.MassiveException;
+import com.nightshadepvp.tournament.Tournament;
+import com.nightshadepvp.tournament.challonge.Challonge;
+import com.nightshadepvp.tournament.cmd.NightShadeTournamentCommand;
+import com.nightshadepvp.tournament.entity.TPlayer;
+import com.nightshadepvp.tournament.utils.ChatUtils;
+
+public class CmdBracket extends NightShadeTournamentCommand {
+
+    public CmdBracket() {
+        this.addAliases("bracket");
+    }
+
+    @Override
+    public void perform() throws MassiveException {
+        TPlayer tPlayer = TPlayer.get(sender);
+        Challonge challonge = Tournament.get().getChallonge();
+
+        if(challonge == null){
+            tPlayer.msg(ChatUtils.message("&cThe bracket hasn't been posted yet!"));
+            return;
+        }
+
+        tPlayer.msg(ChatUtils.message("&bView the bracket here: &f" + challonge.getUrl()));
+    }
+}
