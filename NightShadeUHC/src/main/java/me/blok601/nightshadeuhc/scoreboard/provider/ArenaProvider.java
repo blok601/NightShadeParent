6334d2672e5ee2717d4b67620853d2f71f0fa066
dhillon <dhillonchoudhri@gmail.com>
2020-09-14 22:37:27 -0400

    Try fix on arena scoreboard

--- NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/scoreboard/provider/ArenaProvider.java
+++ NightShadeUHC/src/main/java/me/blok601/nightshadeuhc/scoreboard/provider/ArenaProvider.java
@@ -28,19 +28,19 @@ public class ArenaProvider implements SidebarProvider {
     public List<SidebarEntry> getLines(Player p) {
         ArrayList<SidebarEntry> lines = new ArrayList<>();
         UHCPlayer uhcPlayer = UHCPlayer.get(p);
-        lines.add(new SidebarEntry(ChatUtils.format("&f&m--------------" + ChatColor.AQUA.toString())));
-        lines.add(new SidebarEntry(ChatUtils.format("&fPlayers: &b" + UHCPlayerColl.get().getAllOnline().stream().filter(UHCPlayer::isInArena).count())));
+        lines.add(new SidebarEntry(ChatUtils.format("&f&m--------------") + ChatColor.AQUA.toString()));
+        lines.add(new SidebarEntry(ChatColor.GOLD.toString(), ChatUtils.format("&fPlayers: &b"), ChatColor.AQUA + "" + UHCPlayerColl.get().getAllOnline().stream().filter(UHCPlayer::isInArena).count()));
         lines.add(new SidebarEntry(ChatUtils.format("&f&m--------------" + ChatColor.BLUE.toString())));
         lines.add(new SidebarEntry(ChatUtils.format("&bYour Stats:")));
-        lines.add(new SidebarEntry(ChatUtils.format("&fKills: &b" + uhcPlayer.getArenaKills())));
-        lines.add(new SidebarEntry(ChatUtils.format("&fDeaths: &b" + uhcPlayer.getArenaDeaths())));
-        lines.add(new SidebarEntry(ChatUtils.format("&fHighest KillStreak: &b" + uhcPlayer.getHighestArenaKillStreak())));
-        lines.add(new SidebarEntry(ChatUtils.format("&fK/DR: &b" + ArenaSession.DECIMAL_FORMAT.format(uhcPlayer.getArenaKDR()))));
+        lines.add(new SidebarEntry(ChatColor.BLUE.toString(), ChatUtils.format("&fKills: &b"), ChatColor.AQUA + "" + uhcPlayer.getArenaKills()));
+        lines.add(new SidebarEntry(ChatColor.GREEN.toString(), ChatUtils.format("&fDeaths: &b"), ChatColor.AQUA + "" + uhcPlayer.getArenaDeaths()));
+        lines.add(new SidebarEntry(ChatColor.YELLOW.toString(), ChatUtils.format("&fHighest KillStreak: &b"), ChatColor.AQUA + "" + uhcPlayer.getHighestArenaKillStreak()));
+        lines.add(new SidebarEntry(ChatColor.DARK_GREEN.toString(), ChatUtils.format("&fK/DR: &b"), ChatColor.AQUA + "" + ArenaSession.DECIMAL_FORMAT.format(uhcPlayer.getArenaKDR())));
         lines.add(new SidebarEntry(ChatUtils.format("&f&m--------------" + ChatColor.DARK_BLUE.toString())));
         lines.add(new SidebarEntry(ChatUtils.format("&bCurrent Session:")));
-        lines.add(new SidebarEntry(ChatUtils.format("&fKills: &b" + uhcPlayer.getArenaSession().getKills())));
-        lines.add(new SidebarEntry(ChatUtils.format("&fDeaths: &b" + uhcPlayer.getArenaSession().getDeaths())));
-        lines.add(new SidebarEntry(ChatUtils.format("&fKillStreak: &b" + uhcPlayer.getArenaSession().getKillstreak())));
+        lines.add(new SidebarEntry(ChatColor.BLACK.toString(), ChatUtils.format("&fKills: &b"), ChatColor.AQUA + "" + uhcPlayer.getArenaSession().getKills()));
+        lines.add(new SidebarEntry(ChatColor.DARK_GRAY.toString(), ChatUtils.format("&fDeaths: &b"), ChatColor.AQUA + "" + uhcPlayer.getArenaSession().getDeaths()));
+        lines.add(new SidebarEntry(ChatColor.LIGHT_PURPLE.toString(), ChatUtils.format("&fKillStreak: &b"), ChatColor.AQUA + "" + uhcPlayer.getArenaSession().getKillstreak()));
         lines.add(new SidebarEntry(ChatUtils.format("&f&m--------------")));
         lines.add(new SidebarEntry(ChatUtils.format("&bdiscord.nightshadepvp.com")));
         return lines;
